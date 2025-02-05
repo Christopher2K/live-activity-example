6ffe3561b1220a7e68fadaba4fd11bfa4f73a30e
Christopher N. KATOYI KABA <me@christopher2k.dev>
2025-02-04 22:37:42 -0500

    chore: clean project

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default function Index() {
+  return <View style={styles.container}></View>;
+}
