a42801c02bff02b25f9656a1ad4496f02fb0e604
Christopher N. KATOYI KABA <me@christopher2k.dev>
2025-02-04 22:37:42 -0500

    chore: clean project fix: add expo target plugin feat: add live activity target feat: add native module feat: clean up and write module feat: working!

---
+++ app.config.ts
@@ -0,0 +1,45 @@
+import "ts-node/register"; // Add this to import TypeScript files
+import { ExpoConfig } from "expo/config";
+
+const config: ExpoConfig = {
+  name: "live-activity",
+  slug: "live-activity",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "live-activity",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.christopher.live-activity",
+    appleTeamId: "2YELN5S6P5",
+    infoPlist: {
+      NSSupportsLiveActivities: true,
+    },
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+      },
+    ],
+    "@bacons/apple-targets",
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+};
+
+export default config;
